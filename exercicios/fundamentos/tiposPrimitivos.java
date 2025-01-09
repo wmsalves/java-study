cf6d9e0caec959702b6d86926c95e55cf2485be1
wemer <wemersoncontas22@gmail.com>
2025-01-09 19:32:08 -0300

    feat: primitives types example.

---
+++ exercicios/fundamentos/tiposPrimitivos.java
@@ -0,0 +1,38 @@
+package exercicios.fundamentos;
+
+public class tiposPrimitivos {
+
+    public static void main(String[] args) {
+        // Informações do funcionario
+
+        // numéricos inteiros
+        byte anosDeEmpresa = 21;
+        short numeroDeVoos = 242;
+        int id = 63289;
+        long pontosAcumulados = 5_122_245_113L;
+
+        // numéricos reais
+        float salario = 12_452.40F;
+        double vendasAcumuladas = 3_811_697_213.01;
+
+        // booleano
+        boolean estaDeFerias = false; // true
+
+        // caractere
+        char status = 'A'; // ativo
+
+        // dia de empresa
+        System.out.println(anosDeEmpresa * 365);
+
+        // numero de viagens
+        System.out.println(numeroDeVoos / 2);
+
+        // Pontos por real
+        System.out.println(pontosAcumulados / vendasAcumuladas);
+
+        System.out.println(id + ": ganha -> " + salario);
+        System.out.println("Férias? " + estaDeFerias);
+        System.out.println("Status: " + status);
+    }
+}
+
