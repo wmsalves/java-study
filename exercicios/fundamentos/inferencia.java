a17f79957769f67770f06981310f50a0ad44668b
wemer <wemersoncontas22@gmail.com>
2025-01-09 07:39:21 -0300

    feat: simple inference example.

---
+++ exercicios/fundamentos/inferencia.java
@@ -0,0 +1,36 @@
+package exercicios.fundamentos;
+
+public class inferencia {
+
+    public static void main(String[] args) {
+
+            double a = 5.5; // declaração e inicialização
+            System.out.println(a);
+
+            a = 20;
+            System.out.println(a);
+
+            // a = "...";
+
+            var b = 5.5;
+            System.out.println(b);
+
+            var c = "Wemerson";
+            System.out.println(c);
+
+            c = "Wemerson Alves";
+            System.out.println(c);
+
+            // c = 4.5;
+
+            double d; // variável declarada
+            d = 123.65; // variável inicializada
+            System.out.println(d); // usada!
+
+            var e = 123.45;
+            System.out.println(e);
+            var f = 12; // int
+            // f = 12.01;
+            System.out.println(f);
+    }
+}
