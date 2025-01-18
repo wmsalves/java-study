fb1fbdfbcc31cd4ff16a44755ca75ca43b8dc11d
wemer <wemersoncontas22@gmail.com>
2025-01-18 09:47:42 -0300

    feat: type string equals.

---
+++ exercicios/fundamentos/tipoStringEquals.java
@@ -0,0 +1,22 @@
+package exercicios.fundamentos;
+
+import java.util.Scanner;
+
+public class tipoStringEquals {
+
+    public static void main(String[] args) {
+        System.out.println("5" == "5");
+
+        String s1 = new String("5");
+        System.out.println("5" == s1);
+        System.out.println("5".equals(s1));
+
+        Scanner entrada = new Scanner(System.in);
+
+        String s2 = entrada.next();
+        System.out.println("5" == s2.trim());
+        System.out.println("5".equals(s2.trim()));
+
+        entrada.close();
+    }
+}
