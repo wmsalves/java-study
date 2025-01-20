6319636088db7425d636849fafd23e5a3460f1f7
wemer <wemersoncontas22@gmail.com>
2025-01-20 20:16:02 -0300

    feat: logical operators challenge.

---
+++ exercicios/fundamentos/operadores/logicosDesafio.java
@@ -0,0 +1,22 @@
+package exercicios.fundamentos.operadores;
+
+public class logicosDesafio {
+
+    public static void main(String[] args) {
+        
+        boolean trabalho1 = false;
+        boolean trabalho2 = false;
+
+        boolean comprouTV50 = trabalho1 && trabalho2;
+        boolean comprouTV32 = trabalho1 ^ trabalho2;
+        boolean comprouSorvete = trabalho1 || trabalho2;
+
+        // Operador Unário!
+        boolean maisSaudavel = !comprouSorvete;
+
+        System.out.println("Comprou TV 50\"? " + comprouTV50);
+        System.out.println("Comprou TV 32\"? " + comprouTV32);
+        System.out.println("Comprou Sorvete? " + comprouSorvete);
+        System.out.println("Mais saudável? " + maisSaudavel);
+    }
+}
