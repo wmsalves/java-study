0dc7136861112e2b9ffa8383dd1ca7fc9830f9d3
wemer <wemersoncontas22@gmail.com>
2025-01-20 20:14:59 -0300

    feat: logical operators challenge.

---
+++ exercicios/fundamentos/operadores/logicos.java
@@ -0,0 +1,34 @@
+package exercicios.fundamentos.operadores;
+
+public class logicos {
+    public static void main(String[] args) {
+
+        boolean condicao1 = true;
+        boolean condicao2 = 3 > 7;
+
+        System.out.println(condicao1 && !condicao2);
+        System.out.println(condicao1 || condicao2);
+        System.out.println(condicao1 ^ condicao2);
+        System.out.println(!!condicao1);
+        System.out.println(!condicao2);
+
+        System.out.println("Tabela verdade E (AND)");
+        System.out.println(true && true);
+        System.out.println(true && false);
+
+
+        System.out.println("\nTabela verdade OU (OR)");
+        System.out.println(false || true);
+        System.out.println(false || false);
+
+        System.out.println("\nTabela verdade OU Exclusivo (XOR)");
+        System.out.println(true ^ true);
+        System.out.println(true ^ false);
+        System.out.println(false ^ true);
+        System.out.println(false ^ false);
+
+        System.out.println("\nTabela verdade NOT");
+        System.out.println(!true);
+        System.out.println(!false);
+    }
+}
