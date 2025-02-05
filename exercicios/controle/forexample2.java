7c2b06f8df3dfa5dac252377bd92a1640bc92225
wemer <wemersoncontas22@gmail.com>
2025-02-05 15:22:14 -0300

    feat: add examples of using the for loop

---
+++ exercicios/controle/forexample2.java
@@ -0,0 +1,10 @@
+package exercicios.controle;
+
+public class forexample2 {
+    public static void main(String[] args) {
+
+        for (int contador = 20; contador >= 5; contador -= 3) {
+            System.out.printf("Valor atual: %d\n", contador);
+        }
+    }
+}
