f58cc34925b56f92bfddbdc49aeabcea408347e8
wemer <wemersoncontas22@gmail.com>
2025-02-06 08:38:44 -0300

    feat: implement 2 while loop examples and solve 1 challenge

---
+++ exercicios/controle/whileDeterminado.java
@@ -0,0 +1,12 @@
+package exercicios.controle;
+
+public class whileDeterminado {
+    public static void main(String[] args) {
+
+        int valor = 3;
+        while (valor <= 30) {
+            System.out.printf("Valor atual: %d\n", valor);
+            valor += 3;
+        }
+    }
+}
