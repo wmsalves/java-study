0e74cf0d0e9522b3cfb4cde82617e06238c2b590
wemer <wemersoncontas22@gmail.com>
2025-02-07 08:16:52 -0300

    feat: implement 2 switch and doWhile example.

---
+++ exercicios/controle/doWhile.java
@@ -0,0 +1,22 @@
+package exercicios.controle;
+
+import java.util.Scanner;
+
+public class doWhile {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String resposta = "";
+
+        do {
+            System.out.println("Para continuar, diga a palavra secreta...");
+            System.out.print("Deseja encerrar? ");
+            resposta = input.nextLine();
+        } while (!resposta.equalsIgnoreCase("sim"));
+
+        System.out.println("Encerrando o programa. Até logo!");
+        input.close();
+    }
+}
+
