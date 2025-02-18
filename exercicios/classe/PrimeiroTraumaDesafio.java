328973188386724709e752adb88a3f94f6c71148
wemer <wemersoncontas22@gmail.com>
2025-02-18 13:59:33 -0300

    feat: create with test examples

---
+++ exercicios/classe/PrimeiroTraumaDesafio.java
@@ -0,0 +1,14 @@
+package exercicios.classe;
+
+public class PrimeiroTraumaDesafio {
+    int a = 3;
+    static int b = 4;
+
+    public static void main(String[] args) {
+
+        PrimeiroTraumaDesafio p = new PrimeiroTraumaDesafio();
+        System.out.println(p.a);
+
+        System.out.println(b);
+    }
+}
