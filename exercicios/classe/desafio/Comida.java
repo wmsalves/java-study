199d640f64467d765303095e6d71a106310457ba
wemer <wemersoncontas22@gmail.com>
2025-02-18 14:02:12 -0300

    feat: create Comida, Jantar, Pessoa classes with test examples

---
+++ exercicios/classe/desafio/Comida.java
@@ -0,0 +1,12 @@
+package exercicios.classe.desafio;
+
+public class Comida {
+
+    String nome;
+    double peso;
+
+    Comida(String nome, double peso) {
+        this.nome = nome;
+        this.peso = peso;
+    }
+}
---
+++ exercicios/classe/desafio/Jantar.java
@@ -0,0 +1,19 @@
+package exercicios.classe.desafio;
+
+public class Jantar {
+    public static void main(String[] args) {
+
+        Comida c1 = new Comida("Arroz", 0.180);
+        Comida c2 = new Comida("Feijao", 0.300);
+
+        Pessoa p = new Pessoa("João", 99.8);
+
+        System.out.println(p.apresentar());
+        p.comer(c1);
+
+        System.out.println(p.apresentar());
+        p.comer(c2);
+
+        System.out.println(p.apresentar());
+    }
+}
