783c147d41fa0d47b5d2451780a499bcba576341
wemer <wemersoncontas22@gmail.com>
2025-02-20 19:09:07 -0300

    feat: add HashSet example with Usuario class

---
+++ exercicios/colecoes/Hash.java
@@ -0,0 +1,15 @@
+package exercicios.colecoes;
+
+public class Hash {
+    public static void main(String[] args) {
+
+        HashSet<Usuario> usuarios = new HashSet<Usuario>();
+
+        usuarios.add(new Usuario("Pedro"));
+        usuarios.add(new Usuario("Ana"));
+        usuarios.add(new Usuario("Guilherme"));
+
+        boolean resultado = usuarios.contains(new Usuario("Guilherme"));
+        System.out.println(resultado);
+    }
+}
