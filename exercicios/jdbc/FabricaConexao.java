285ed5fb7a4d0fd7a887d0528efafbdea1f471e1
Infinity <wemersoncontas22@gmail.com>
2025-09-08 13:33:56 -0300

    feat: add CriarTabelaPessoas to create pessoas table via JDBC

--- exercicios/jdbc/FabricaConexao.java
+++ exercicios/jdbc/FabricaConexao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class FabricaConexao {
     public static Connection getConexao() {
         try {
-            final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
+            final String url = "jdbc:mysql://localhost/java_study?verifyServerCertificate=false&useSSL=true";
             final String usuario = "root";
             final String senha = "12345678";
 
