8a0277cfa3323a5d1023f4b747f19cfffd42fcff
Infinity <wemersoncontas22@gmail.com>
2025-09-08 13:25:37 -0300

    feat: add FabricaConexao class for JDBC connection management

--- exercicios/jdbc/CriarBanco.java
+++ exercicios/jdbc/CriarBanco.java
@@ -8,17 +8,17 @@ import java.sql.Statement;
 public class CriarBanco {
 
 	public static void main(String[] args) throws SQLException {
-		
+
 		final String url = "jdbc:mysql://localhost?verifyServerCertificate=false&useSSL=true";
 		final String usuario = "root";
 		final String senha = "12345678";
-		
+
 		Connection conexao = DriverManager
 				.getConnection(url, usuario, senha);
-		
+
 		Statement stmt = conexao.createStatement();
 		stmt.execute("CREATE DATABASE IF NOT EXISTS curso_java");
-		
+
 		System.out.println("Banco criado com sucesso!");
 		conexao.close();
 	}
