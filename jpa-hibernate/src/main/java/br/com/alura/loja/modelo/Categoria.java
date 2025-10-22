2210e6ba92dd0a660ca3b0edd97cfcce54f52a6e
Infinity <wemersoncontas22@gmail.com>
2025-10-22 13:30:36 -0300

    test(jpa): demonstrate entity lifecycle states (transient, managed, detached)

--- jpa-hibernate/src/main/java/br/com/alura/loja/modelo/Categoria.java
+++ jpa-hibernate/src/main/java/br/com/alura/loja/modelo/Categoria.java
@@ -15,8 +15,18 @@ public class Categoria {
     private Long id;
     private String nome;
 
+    public Categoria() {
+    }
+
     public Categoria(String nome) {
         this.nome = nome;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
