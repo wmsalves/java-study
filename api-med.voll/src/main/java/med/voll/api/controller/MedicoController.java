1b539f3d9d2dd5edf590d48fed9a2e5b5fb602a8
Infinity <wemersoncontas22@gmail.com>
2025-10-24 09:08:04 -0300

    refactor(api): Standardize controller responses using ResponseEntity

--- api-med.voll/src/main/java/med/voll/api/controller/MedicoController.java
+++ api-med.voll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,26 +19,35 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+    public ResponseEntity cadastrar(@RequestBody DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
+
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
-        return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+    public ResponseEntity<Page<DadosListagemMedico>> listar(
+            @PageableDefault(size = 10, sort = { "nome" }) Pageable paginacao) {
+        var page = repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
+
+        return ResponseEntity.ok(page);
     }
 
     @PutMapping
     @Transactional
-    public void atualizar(@RequestBody DadosAtualizacaoMedico dados) {
+    public ResponseEntity atualizar(@RequestBody DadosAtualizacaoMedico dados) {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
+
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
     }
 
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluir(@PathVariable Long id) {
+    public ResponseEntity excluir(@PathVariable Long id) {
         var medico = repository.getReferenceById(id);
         medico.excluir();
+
+        return ResponseEntity.noContent().build();
     }
 }
--- api-med.voll/src/main/java/med/voll/api/endereco/DadosEndereco.java
+++ api-med.voll/src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -3,21 +3,16 @@ package med.voll.api.endereco;
 import jakarta.validation.constraints.*;
 
 public record DadosEndereco(
-        @NotBlank
-        String logradouro,
+        @NotBlank String logradouro,
 
-        @NotBlank
-        String bairro,
+        @NotBlank String bairro,
 
-        @NotBlank
-        @Pattern(regexp = "\\d{8}")
-        String cep,
+        @NotBlank @Pattern(regexp = "\\d{8}") String cep,
 
-        @NotBlank
-        String cidade,
-        
-        @NotBlank
-        String uf,
+        @NotBlank String cidade,
+
+        @NotBlank String uf,
         String complemento,
         String numero) {
+
 }
