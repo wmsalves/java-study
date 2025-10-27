d14f4a7f8c8f066876a17ee503a1c30c5b943f0b
Infinity <wemersoncontas22@gmail.com>
2025-10-27 09:37:35 -0300

    refactor(auth): wrap login token in DTO and externalize secret via @Value

--- api-med.voll/src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ api-med.voll/src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
 import med.voll.api.domain.usuario.DadosAutenticacao;
 import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
 import med.voll.api.infra.security.TokenService;
 
 @RestController
@@ -27,10 +28,11 @@ public class AutenticacaoController {
 
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var autentication = manager.authenticate(token);
+        var authenticationtToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationtToken);
 
-        return ResponseEntity
-                .ok(tokenService.gerarToken((Usuario) autentication.getPrincipal()));
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
